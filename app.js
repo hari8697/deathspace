e3ef2973e81fd8fcb24253bf663d0051083f7a5f
Hari Krishnan <hari8697@gmail.com>
2019-12-12 11:30:41 +0530

    commit 4

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var myvid = document.getElementById('myvideo');
+
+window.onunload = function(){ window.scrollTo(0,0); }
 var myvids = [
   "video.mp4",
   "loop.mp4"
   ];
 var activeVideo = 0;
-
 myvid.addEventListener('ended', function(e) {
   // update the new active video index
+  myvid.pause();
   activeVideo = (++activeVideo) % myvids.length;
 
   // update the video source and play
   myvid.src = myvids[activeVideo];
   myvid.loop = true;
+  myvid.play();
 });
