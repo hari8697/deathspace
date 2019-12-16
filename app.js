01729d4f285382ed291372e4a4bb6df8ffb50d36
Hari Krishnan <hari8697@gmail.com>
2019-12-16 05:52:19 +0530

    commit 5

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
 var myvid = document.getElementById('myvideo');
-
-window.onunload = function(){ window.scrollTo(0,0); }
 var myvids = [
-  "video.mp4",
-  "loop.mp4"
+  "./img/video.mp4",
+  "./img/loop.mp4"
   ];
 var activeVideo = 0;
 myvid.addEventListener('ended', function(e) {
   // update the new active video index
-  myvid.pause();
   activeVideo = (++activeVideo) % myvids.length;
 
   // update the video source and play
   myvid.src = myvids[activeVideo];
   myvid.loop = true;
   myvid.play();
+});
+
+$(document).ready(function () {
+  var i = 0;
+  $("#button").click(function() {
+      scrollDown();
+      i++;
+  });
+
+  function scrollDown() {
+    $('html, body').animate({
+      scrollTop: $(".nav").offset().top
+    }, 0);
+  }
+
+  $(window).scroll(function() {
+    if($(window).scrollTop() < 10)
+    {
+      i = 0;
+    }
+    if($(window).scrollTop() > 10){
+      if (i == 0) {
+        scrollDown();
+        i++;
+      }
+    }
+  });
+
+  $(window).load(function() {
+    $('html, body').animate({
+      scrollTop: $(".s1").offset().top
+    }, 0);
+  });
 });
